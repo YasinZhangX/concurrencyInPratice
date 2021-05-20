3aa1ea811e732623d5b47f31748b25b578c428d5
YasinZhang <271155397@qq.com>
2021-05-20 13:44:45 +0800

    add thread pool policy demo

---
+++ src/main/java/com/github/yasinzhangx/threadpool/BoundedExecutor.java
@@ -0,0 +1,35 @@
+package com.github.yasinzhangx.threadpool;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.Semaphore;
+
+/**
+ * @author Yasin Zhang
+ */
+public class BoundedExecutor {
+
+    private final Executor exec;
+    private final Semaphore semaphore;
+
+    public BoundedExecutor(Executor exec, int bound) {
+        this.exec = exec;
+        this.semaphore = new Semaphore(bound);
+    }
+
+    public void submitTask(final Runnable command) throws InterruptedException {
+        semaphore.acquire();
+        try {
+            exec.execute(() -> {
+                try {
+                    command.run();
+                } finally {
+                    semaphore.release();
+                }
+            });
+        } catch (RejectedExecutionException e) {
+            semaphore.release();
+        }
+    }
+
+}
